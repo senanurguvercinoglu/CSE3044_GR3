2c7f1706da5d06703ba317cf707b4614536775f4
cemanaral <cemanaral425@gmail.com>
2022-04-30 23:08:48 +0300

    ingredient model / set ingredient action for recipe endpoint

--- backend/api_project/api_app/serializers.py
+++ backend/api_project/api_app/serializers.py
@@ -1,14 +1,22 @@
-from tkinter.tix import Tree
 from django.contrib.auth.models import User, Group
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import serializers
 
-from api_project.api_app.models import Recipe
+from api_project.api_app.models import Ingredient, Recipe
 
+class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['url', 'name']
 
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+    ingredients = IngredientSerializer(read_only=True, many=True)
+
+
     class Meta:
         model = Recipe
-        fields = ['url', 'user', 'name', 'recipe_description', 'likes', 'dislikes', 'calorie']
+        fields = ['url', 'user', 'name', 'recipe_description', 'likes', 'dislikes', 'calorie', 'ingredients']
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     recipe = RecipeSerializer(many=True, required=False)
--- backend/api_project/api_app/views.py
+++ backend/api_project/api_app/views.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from api_project.api_app.models import Recipe
 
-from .serializers import RecipeSerializer, UserSerializer
+from api_project.api_app.models import Ingredient, Recipe
+
+from .serializers import IngredientSerializer, RecipeSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -21,3 +24,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
 
+    @action(detail=True, methods=['post'], url_path=r'set_ingredient/(?P<ingredient_id>\d+)')
+    def set_ingredient(self, request, pk, ingredient_id):
+        ingredient = Ingredient.objects.get(pk=ingredient_id)
+        recipe = Recipe.objects.get(pk=pk)
+
+        recipe.ingredients.add(ingredient)
+        recipe.save()
+
+        serializer = self.get_serializer(recipe)
+        return Response(serializer.data)
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
