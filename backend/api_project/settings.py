2ce70052640038db6706a660e78a9254b13dfc31
cemanaral <cemanaral425@gmail.com>
2022-05-16 22:13:16 +0300

    disabled pagination

--- backend/api_project/settings.py
+++ backend/api_project/settings.py
@@ -131,10 +131,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#    'PAGE_SIZE': 10
+# }
 
 # for react frontend server
 CORS_ALLOW_ALL_ORIGINS = True
