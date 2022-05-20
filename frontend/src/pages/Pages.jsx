c417d0054dcf70aaf4a416c77d31a8d1aeddffbc
cemanaral <cemanaral425@gmail.com>
2022-05-20 19:57:05 +0300

    fake apide calisiyor

--- frontend/src/pages/Pages.jsx
+++ frontend/src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Home';
 import Cuisine from './Cuisine';
 import Searched from './Searched';
+import Recipe from './Recipe';
 import {Route, Routes,useLocation} from 'react-router-dom';
 import {AnimatePresence} from 'framer-motion';
 
@@ -14,7 +15,8 @@ function Pages() {
             <Route path='/' element={<Home />} /> 
             <Route path='/cuisine/:type' element={<Cuisine />} /> 
             <Route path='/searched/:search' element={<Searched />} /> 
-          
+            <Route path='/recipe/:name' element={<Recipe />} /> 
+
 
 
 
