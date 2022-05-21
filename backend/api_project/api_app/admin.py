434d3a3567166aa5cd51562bee62e21911f3aae9
cemanaral <cemanaral425@gmail.com>
2022-05-21 16:50:39 +0300

    added ingredient and utensil to admin panel

--- backend/api_project/api_app/admin.py
+++ backend/api_project/api_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from api_project.api_app.models import Recipe
+from api_project.api_app.models import Recipe, Ingredient, Utensil
 
 # Register your models here.
 admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Utensil)
+
